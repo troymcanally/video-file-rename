c9e448a2041b80575cb4a05bd9a233b3e67499e3
Troy <troymcanally@aussieb.com.au>
2019-12-30 13:29:32 +1030

    Script broke, fixed issue with using list before sort

--- rename.py
+++ rename.py
@@ -2,7 +2,8 @@
 # Author: Trojan
 
 import os
-from natsort import natsorted #using this module to natural sort the files by episode number.
+# using this module to natural sort the files by episode number.
+import natsort as ns
 
 # get the name of the show and the current season from the user
 show_name = input("What is the show's name?")
@@ -11,16 +12,16 @@ current_season = input("What is the current season?")
 # gets the list of files in the anime folder for renaming,sets the episode counter and sorts the files by episode using a natural sort on the name string of the files
 path = os.path.join(r"C:\Users\troym\Downloads\Video")
 filenames = os.listdir(path)
-natsorted(filenames)
+filesort = ns.natsorted(filenames)
 episode_counter = 1
 
-# if statement to check if filenames list is empty
-if filenames:
+# if statement to check if filesort list is empty
+if filesort:
     # loops through all the files in the video folder and renames each one with the show name, the season, and the iterated episode number, if no files prints error
-    for files in filenames:
+    for files in filesort:
         filename, file_extension = os.path.splitext(files)
         os.rename(path + "\\" + filename + file_extension, path + "\\" + show_name + " " + "s" +
-            str(current_season).zfill(2) + "e" + str(episode_counter).zfill(2) + file_extension)
+                  str(current_season).zfill(2) + "e" + str(episode_counter).zfill(2) + file_extension)
         episode_counter = episode_counter + 1
 else:
     print("Opps! No files in the folder to rename.")
